554492e6733ae815463c0edb15efd500c5eb57b6
L200170147 <47980656+l200170147@users.noreply.github.com>
2019-10-21 21:06:49 +0700

    Add files via upload

---
+++ MODUL_5_Tugas/update.php
@@ -0,0 +1,30 @@
+<?php
+	$conn= mysqli_connect('localhost', 'root', '','informatika');
+	
+	$nim=$_POST['nim'];
+	$nim2=$_POST['nim2'];
+	$nama=$_POST['nama'];
+	$kelas=$_POST['kelas'];
+	$alamat=$_POST['alamat'];
+	$submit=$_POST['submit'];
+	$update="UPDATE mahasiswa set NIM='$nim', Nama='$nama', Kelas='$kelas', Alamat='$alamat' WHERE NIM='$nim2' ";
+	
+	if($submit){
+		if ($nim=''){
+			echo "NIM tidak boleh kosong, diisi dulu";
+		}elseif ($nama=''){
+			echo "Nama tidak boleh kosong";
+		}elseif ($alamat=''){
+			echo "Alamat tidak boleh kosong";
+		}if ($nim=''){
+			echo "NIM tidak boleh kosong, diisi dulu";
+		}else
+			mysqli_query($conn,$update);
+			echo "
+			<script>
+			alert('data berhasil di update');
+			document.location.href='form.php';
+			</script>";
+		}
+	
+?>
